12f06e8194667d85a8439093a0388081617f496c
gordomathurin <mathuringordon@gmail.com>
2021-10-27 23:33:56 -0400

    added javaFX, simple gui

--- src/com/gordon_workspace/ProcessFile.java
+++ src/com/gordon_workspace/ProcessFile.java
@@ -1,6 +1,21 @@
 package com.gordon_workspace;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
@@ -20,7 +35,7 @@ public class ProcessFile {
     public void readWords() {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            bufferedReader.lines().map(pointer -> pointer.replaceAll("[^a-zA-Z]", " ").toLowerCase().split("\\s+")).flatMap(Arrays::stream).map(String::trim).forEach(word -> {
+            bufferedReader.lines().map(pointer -> pointer.replaceAll("[^a-zA-Z0-9]", " ").toLowerCase().replaceAll("^ +| +$|( )+", "$1").split("\\s+")).flatMap(Arrays::stream).map(String::trim).forEach(word -> {
                 if (wordHolder.containsKey(word)) {
                     wordHolder.put(word, wordHolder.get(word) + 1);
                 } else {
@@ -41,4 +56,5 @@ public class ProcessFile {
         return wordHolder;
     }
 
-}
+    }
+
